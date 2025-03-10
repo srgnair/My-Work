38b41b19ba6f1d93b8561b845abaab8bc153fbe5
白金愛里 <air.shirogane@gmail.com>
2025-03-11 05:43:08 +0900

    first commit

---
+++ src/resources/views/top.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="テキストテキストテキスト">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <title>My Work</title>
+</head>
+
+<body>
+</body>
+</html>
